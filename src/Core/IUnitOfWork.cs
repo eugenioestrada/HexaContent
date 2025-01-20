7d88b3568c8210f3c782536c8be4443d6270138d
Eugenio Estrada <eugenio.estrada@gmail.com>
2025-01-20 09:34:40 +0100

    Add IUnitOfWork interface for repository management
    Introduce IUnitOfWork interface in HexaContent.Core namespace.
    Encapsulates repository interfaces and provides SaveChangesAsync method.

---
+++ src/Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using HexaContent.Core.Repositories;
+
+namespace HexaContent.Core;
+
+public interface IUnitOfWork
+{
+	IArticlesRepository Articles { get; }
+	IArticleMetaRepository ArticleMeta { get; }
+	IArticleSectionsRepository ArticleSections { get; }
+	IAuthorsRepository Authors { get; }
+	IAuthorMetaRepository AuthorMeta { get; }
+	ISectionsRepository Sections { get; }
+	ISectionMetaRepository SectionMeta { get; }
+	IMediaRepository Media { get; }
+	IMediaMetaRepository MediaMeta { get; }
+	ValueTask<int> SaveChangesAsync();
+}
