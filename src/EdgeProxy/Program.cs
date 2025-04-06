6f464892beb7bc7b4f4e2f09a831ca052cb8a27a
Eugenio Estrada <eugenio.estrada@gmail.com>
2025-04-06 10:17:55 +0200

    Update routing and caching in Program.cs
    Changed initial route `RouteId` from "bucket" to "root" and added multiple routes for handling file extensions and catch-all paths. Updated request handling logic based on `RouteId` to differentiate path prefixing. Introduced a new output cache policy with a 20-second expiration, and integrated output caching into the request pipeline.

--- src/EdgeProxy/Program.cs
+++ src/EdgeProxy/Program.cs
@@ -31,13 +31,67 @@ var bucketName = bucketNameMatch.Groups[1].Value;
 
 List<RouteConfig> routes = [
 	new RouteConfig {
-		RouteId = "bucket",
+		RouteId = "root",
 		ClusterId = "bucket",
 		Match = new RouteMatch
 		{
-			Path = "/{**catch-all}"
+			Path = "/"
 		}
 	},
+	new RouteConfig {
+		RouteId = "with-ext-0",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{filename}.{ext}",
+			Methods = [ "GET" ]
+		}
+	},
+	new RouteConfig {
+		RouteId = "with-ext-1",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{path1}/{filename}.{ext}",
+			Methods = [ "GET" ]
+		}
+	},
+	new RouteConfig {
+		RouteId = "with-ext-2",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{path1}/{path2}/{filename}.{ext}",
+			Methods = [ "GET" ]
+		}
+	},
+	new RouteConfig {
+		RouteId = "with-ext-3",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{path1}/{path2}/{path3}/{filename}.{ext}",
+			Methods = [ "GET" ]
+		}
+	},
+	new RouteConfig {
+		RouteId = "with-ext-4",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{path1}/{path2}/{path3}/{path4}/{filename}.{ext}",
+			Methods = [ "GET" ]
+		}
+	},
+	new RouteConfig {
+		RouteId = "all",
+		ClusterId = "bucket",
+		Match = new RouteMatch
+		{
+			Path = "/{**catch-all}",
+			Methods = [ "GET" ]
+		}
+	}
 ];
 
 List<ClusterConfig> clusters = [
@@ -56,10 +110,32 @@ builder.Services.AddReverseProxy()
 	.LoadFromMemory(routes, clusters)
 	.AddTransforms(builderContext =>
 	{
-		builderContext.AddPathPrefix($"/{bucketName}");
+		if (builderContext.Route.RouteId != "root")
+		{
+			builderContext.AddPathPrefix($"/{bucketName}");
+		}
+		else
+		{
+			builderContext.AddPathSet($"/{bucketName}/test.html");
+		}
+
+		if (builderContext.Route.RouteId == "all")
+		{
+			builderContext.AddRequestTransform(async (transformContext) =>
+			{
+				transformContext.Path = new PathString($"{transformContext.Path}.html");
+			});
+		}
 	});
 
+builder.Services.AddOutputCache(options =>
+{
+	options.AddPolicy("customPolicy", builder => builder.Expire(TimeSpan.FromSeconds(20)));
+});
+
 var app = builder.Build();
+
+app.UseOutputCache();
 
 app.MapReverseProxy();
 
