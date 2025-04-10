e64e1f5430c409aa20b40afb33ff00c7ce1839f9
Eugenio Estrada <eugenio.estrada@gmail.com>
2025-04-10 16:19:41 +0200

    Refactor Worker constructor for clarity
    Simplify the Worker class constructor by using parameter
    initialization for logger and minioFactory fields,
    enhancing code readability and conciseness.

--- src/StaticForge/Worker.cs
+++ src/StaticForge/Worker.cs
@@ -5,18 +5,12 @@ using HexaContent.Minio.Client;
 
 namespace HexaContent.StaticForge;
 
-public class Worker : BackgroundService
+public class Worker(ILogger<Worker> logger, MinioFactory minioFactory) : BackgroundService
 {
-    private readonly ILogger<Worker> _logger;
-	private readonly MinioFactory _minioFactory;
+    private readonly ILogger<Worker> _logger = logger;
+	private readonly MinioFactory _minioFactory = minioFactory;
 
-	public Worker(ILogger<Worker> logger, MinioFactory minioFactory)
-    {
-        _logger = logger;
-        _minioFactory = minioFactory;
-	}
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
