9a99530f44d47f82030cd6e5bd08af0641ada980
Eugenio Estrada <eugenio.estrada@gmail.com>
2025-04-10 10:58:21 +0200

    Add rendering service and template support
    Implement IRenderService for template rendering using Scriban.
    Register service in DI and add Scriban package.

---
+++ src/Services/RenderService.cs
@@ -0,0 +1,25 @@
+﻿using HexaContent.Core.Services;
+using Scriban;
+using System.Collections.Concurrent;
+
+namespace HexaContent.Services;
+
+public class RenderService : IRenderService
+{
+	private static readonly ConcurrentDictionary<string, Template> _templates = new();
+
+	public ValueTask<string> RenderAsync<T>(string template, T model) where T : class => GetTemplate(template).RenderAsync(model);
+
+	private Template GetTemplate(string template)
+	{
+		if (_templates.TryGetValue(template, out var cachedTemplate))
+		{
+			return cachedTemplate;
+		}
+
+		var newTemplate = Template.Parse(template);
+		_templates.TryAdd(template, newTemplate);
+
+		return newTemplate;
+	}
+}
