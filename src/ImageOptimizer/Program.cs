72b04dc3c219019766bb9ff91c6627d665922557
Eugenio Estrada <eugenio.estrada@gmail.com>
2025-04-26 20:36:54 +0200

    Temporarily disable ImageSharp service configuration
    Commented out the addition of the ImageSharp service and middleware in Program.cs. The `CustomImageProvider` remains active, but its surrounding context is now disabled for potential future re-enablement.

--- src/ImageOptimizer/Program.cs
+++ src/ImageOptimizer/Program.cs
@@ -3,13 +3,13 @@ using SixLabors.ImageSharp.Web.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddImageSharp(options =>
+/* builder.Services.AddImageSharp(options =>
 {
-}).AddProvider<CustomImageProvider>();
+}).AddProvider<CustomImageProvider>();*/
 
 var app = builder.Build();
 
-app.UseImageSharp();
+// app.UseImageSharp();
 
 app.MapGet("/_health", () => "Ok!");
 
